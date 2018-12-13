a1840a2d3f937a85d5e93a891a815e498ac1e7ca
Tiago Marinho <tiago.marinho.797@gmail.com>
2018-12-13 10:52:32 -0200

    Cart model update

--- src/models/cart.js
+++ src/models/cart.js
@@ -4,30 +4,58 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-    id: {
+    email: {
         type: String,
-        required: true
-    }, 
-    
-    nome:{
-        type: String,
-        required: true
-    }, 
-
-    nomeFantasia:{
-        type: String,
-        required: true
-    }, 
-
-    siglaEstado:{
-        type: String,
-        required: true
+        required: true,
+        unique: true
     },
-
-    dataAbertura:{
-        type: String,
-        required: true
+    
+    convenios: {
+        default: undefined,
+        type: [
+            {
+                id: {
+                    type: String,
+                    required: true
+                },
+                nomeConvenente: {
+                    type: String,
+                    required: true
+                },
+                nomeFantasia: {
+                    type: String,
+                    required: true
+                },
+                siglaEstado: {
+                    type: String,
+                    required: true
+                },
+                dataAbertura: {
+                    type: String,
+                    required: true
+                } 
+            }
+        ]
     }
+    // nomeConvenente:{
+    //     type: String
+    
+    // },
+
+    // nomeFantasia:{
+    //     type: String
+    
+    // }, 
+
+    // siglaEstado:{
+    //     type: String
+    
+    // },
+
+    // dataAbertura:{
+    //     type: String
+    
+    // }
 })
 
 module.exports = mongoose.model('Cart', schema);
