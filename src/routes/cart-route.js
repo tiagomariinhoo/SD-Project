33adf520a30d26262e2f96db2cc68a7d54d57d75
Tiago Marinho <tiago.marinho.797@gmail.com>
2018-12-13 10:52:56 -0200

    Cart route

--- src/routes/cart-route.js
+++ src/routes/cart-route.js
@@ -4,7 +4,10 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/cart_controller');
 
-router.get('/', controller.get);
+// router.get('/', controller.get);
+router.get('/index', controller.index);
 router.post('/', controller.post);
+router.put('/:email', controller.put);
+router.delete('/:email', controller.delete);
 
 module.exports = router;
