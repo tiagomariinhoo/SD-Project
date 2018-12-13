12c2d112d78272f59f66e160c49b8d36e9f52e37
Tiago Marinho <tiago.marinho.797@gmail.com>
2018-12-13 10:53:38 -0200

    Config update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo ao Node Store!';
 
 module.exports = {
-    connectionString: "mongodb://tiago:tiago123@ds123454.mlab.com:23454/ndstr",
+    connectionString: "mongodb://tiago:tiago123@ds025419.mlab.com:25419/sd-project",
     sendgridKey: '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',
     containerConnectionString: 'TBD'
 }
